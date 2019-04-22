9a3cd2190a97963bf629fecf78159eeecc8897f1
Rajendra Singh <rajendra.singh@marathonelectric.com>
2019-04-23 00:01:07 +0530

    Change greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = exp();
 const test = exp();
 
 app.get("/", (req, res) => {
-  res.send({ Hi: "There" });
+  res.send({ bye: "buddy" });
 });
 
 const PORT = process.env.PORT || 5001; //Enverment Varialbes which is find port number at Heroku or production. get 5001 port number for
