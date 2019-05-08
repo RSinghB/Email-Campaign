0be602e3b7c9484a3b87a633c0757ebb86f3f1be
Rajendra Singh <rajendra.singh@marathonelectric.com>
2019-05-08 21:37:52 +0530

    tweak google strategy settings

--- servies/passport.js
+++ servies/passport.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.facebookClientID,
       clientSecret: keys.facebookClientSecret,
-      callbackURL: "/auth/facebook/callback"
+      callbackURL: "/auth/facebook/callback",
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       // console.log("Accesss Token ", accessToken);
@@ -51,7 +52,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
 
     (accessToken, refreshToken, profile, done) => {
